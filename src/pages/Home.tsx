9befcc322f131d17abba6b5a70871e95ee076da6
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-03 15:09:54 +0100

    feat: enhance LoginForm with error handling and navigation; update Home page to include CirclePlus button for adding videos

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import FilterSidebar from './../components/FilterSidebar';
 import Header from '../layouts/Header';
 import VideoGrid from '../components/VideoGrid';
+import { CirclePlus } from 'lucide-react';
 
 const Home: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
     return (
         <div>
             <Header />
@@ -16,6 +18,10 @@ const Home: React.FC = () => {
                 <main className="flex-1 overflow-y-auto relative w-full">
                     <VideoGrid />
                 </main>
+                <div className="absolute bottom-0 left-0 right-0 h-1 bg-indigo-600 " title='add video' onClick={()=> setIsOpen(!isOpen)}>
+                    <CirclePlus size={40} strokeWidth={3} className="absolute bottom-2 right-2 text-green-400 "  />
+                </div>
+                {/* Add Video Modal */}
 
             </div>
         </div>
