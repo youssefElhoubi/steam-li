7354c8a6dc1e45cff87610c222afa239638a3c00
youssefElhoubi <elhoubiyoussef@gmail.com>
2026-02-03 11:56:06 +0100

    feat cretaed the home page routed it and made some chnages in the filter side bar

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FilterSidebar from './../components/FilterSidebar';
+
+const Home : React.FC = () => {
+    return (
+        <div>
+            <FilterSidebar/>
+        </div>
+    )
+}
+
+export default Home
